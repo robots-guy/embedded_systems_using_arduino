7a1d8efb937ec52a20f3ae46fb2123cf6041b7f7
Naresh Kumar <nareshkumarindia93@gmail.com>
2023-02-16 13:48:41 +0800

    Create P3_Serial_Communication.ino

---
+++ P3_Serial_Communication.ino
@@ -0,0 +1,12 @@
+void setup()
+{ 
+   //Initializes the Serial communication at 9600 baud rate
+   Serial.begin(9600); 
+
+}
+
+ void loop() 
+{
+Serial.println(" Any Message");  //Prints string in a new line
+delay(100); 
+}
